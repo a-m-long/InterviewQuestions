b3003dbdcd1e6957999cfbbddb9d3eeb8e3cefec
Adriana Long <adriana.michelle.long@gmail.com>
2020-07-22 11:49:44 -0700

    pulled values out of JSON and insterted them in arrays

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { JavaComponent } from './java/java.component';
@@ -10,7 +11,8 @@ import { JavaComponent } from './java/java.component';
     JavaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/java.service.ts
+++ src/app/java.service.ts
@@ -7,13 +7,38 @@ import { HttpClient } from '@angular/common/http';
 export class JavaService {
 
   javaData: JSON;
+  answer: String[]; 
+  question: String[]; 
+  id: number[];
 
 
   constructor(private http: HttpClient) { }
 
   getAllJava() {
-    this.http.get('https://flashcards-23d9f.firebaseio.com/1ehwLfTSExsC-kgy5DBtTG6OEL59rMCmRcUM47364uq4/Sheet1').subscribe(data => {
+    this.http.get('https://flashcards-23d9f.firebaseio.com/1ehwLfTSExsC-kgy5DBtTG6OEL59rMCmRcUM47364uq4/Sheet1.json'
+    ).subscribe(data => {
       let java_data:any = this.javaData = data as JSON;
+
+      for (let i in java_data){
+        let individual = java_data[i];
+        for (let item in individual){
+          //Questions
+          if(item=="Question"){
+            this.question.push(individual[item]);
+          }
+          //filtering answers
+          if (item =="Answer"){
+            this.answer.push(individual[item])
+            console.log(individual[item]);
+          }
+          //to show the number in the list on the frontend
+          if(item =="id"){
+            this.id.push(individual[item]);
+          }
+        }
+      }
+
+      
 
     })
   }
