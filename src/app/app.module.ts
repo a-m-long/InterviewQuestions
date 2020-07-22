9c21785e83b401aae6de813537c01876b578fe09
Adriana Long <adriana.michelle.long@gmail.com>
2020-07-21 21:38:24 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JavaComponent } from './java/java.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JavaComponent
   ],
   imports: [
     BrowserModule
