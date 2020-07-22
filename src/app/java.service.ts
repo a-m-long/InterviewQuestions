6ee82cbe700a73b33272625d0d87861cb63cde3e
Adriana Long <adriana.michelle.long@gmail.com>
2020-07-22 08:07:48 -0700

    added json

--- src/app/java.service.ts
+++ src/app/java.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JavaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllJava() {
+    this.http.get('https://flashcards-23d9f.firebaseio.com/1ehwLfTSExsC-kgy5DBtTG6OEL59rMCmRcUM47364uq4/Sheet1').subscribe(data => {
+
+    })
+  }
+
+
 }
